a2c687227f7841a8b967d5e8d33c65967714c54b
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2022-02-21 19:36:26 +0900

    redirect url 하드 코딩

--- src/Constant/kakao.constant.ts
+++ src/Constant/kakao.constant.ts
@@ -4,8 +4,9 @@ export const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY || "";
 
 export const KAKAO_JAVASCRIPT_KEY = process.env.KAKAO_JAVASCRIPT_KEY || "";
 
-// BEFORE PUBLISH: KAKAO OAUTH REDIRECT URL 교체
+// TODO: SERVICE_URL 왜 자꾸 localhost로 가져오는지
+// BEFORE PUBLISH: REDIRECT URL 설정
 // const KAKAO_OAUTH_REDIRECT_URL = `${SERVICE_URL}/kakao-oauth`;
-const KAKAO_OAUTH_REDIRECT_URL = `http://localhost:3000/kakao-oauth`;
+const KAKAO_OAUTH_REDIRECT_URL = `https://surbay-sprint.netlify.app/kakao-oauth`;
 
 export const KAKAO_OAUTH_REQUST_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_OAUTH_REDIRECT_URL}&response_type=code`;
