5fea7f6eb75396f5eb4f240b049b523de477b71a
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2022-02-21 20:41:09 +0900

    netlify redirect 재설정 - accounts.kakao.com

--- src/Constant/kakao.constant.ts
+++ src/Constant/kakao.constant.ts
@@ -6,7 +6,7 @@ export const KAKAO_JAVASCRIPT_KEY = process.env.KAKAO_JAVASCRIPT_KEY || "";
 
 // TODO: SERVICE_URL 왜 자꾸 localhost로 가져오는지
 // BEFORE PUBLISH: REDIRECT URL 설정
-// const KAKAO_OAUTH_REDIRECT_URL = `${SERVICE_URL}/kakao-oauth`;
+// const KAKAO_OAUTH_REDIRECT_URL = `http://localhost:3000/kakao-oauth`;
 const KAKAO_OAUTH_REDIRECT_URL = `https://surbay-sprint.netlify.app/kakao-oauth`;
 
 export const KAKAO_OAUTH_REQUST_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_OAUTH_REDIRECT_URL}&response_type=code`;
